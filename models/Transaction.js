17f45d3b260417fa99fa6ec953724a61f3cb6e50
Moses Egboh <mosesegboh@yahoo.com>
2022-10-12 15:26:13 +0100

    added jwt token features and added transactions api to the backend

---
+++ models/Transaction.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TransactionSchema = new Schema({
+    email: String,
+    transactionId: String,
+    amount: Number,
+    transactionType: String,
+    date: Date,
+    details: String,
+    secondLegTransactionId: {
+        type: String,
+        required: false,
+    }
+})
+
+const Transaction = mongoose.model('Transaction', TransactionSchema)
+
+module.exports = Transaction;
