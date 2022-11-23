5b7b87699ab370d01419caeae46ac9ad361a2e8e
Moses Egboh <mosesegboh@yahoo.com>
2022-11-23 16:52:55 +0100

    logging for debugging

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -20,7 +20,7 @@ const authMiddleware = (req, res, next) => {
         jwt.verify(token, config.get('ACCESS_TOKEN_SECRET'), (err, decodedToken) => {
             if(err){
                 console.log(err.message);
-                res.json({msg: 'You need to login to access this page 1-invalid token', data: token})
+                res.json({msg: 'You need to login to access this page -invalid token', data: token})
             }else{
                 //console.log(decodedToken)
                 next();
